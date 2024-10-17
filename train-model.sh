b97a0b1570c7d6885334be38753922f96915bfea
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-10-17 07:50:03 +0000

    refactor: rename sentence dataset

---
+++ train-model.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+set -e
+
+# clear training dir
+rm --recursive --verbose tesstrain/data/eng-ground-truth/*.*
+
+# copy data set to training dir
+cp --recursive --verbose --force training_data/special-alphabet/ewe-*/* tesstrain/data/eng-ground-truth
+cp --recursive --verbose --force training_data/special-alphabet/sentence/* tesstrain/data/eng-ground-truth
+
+cd tesstrain;
+
+make training MODEL_NAME=eng
+
+# copy the final model
+cp --verbose --force data/eng.traineddata .
+
+echo "Training completed!";
